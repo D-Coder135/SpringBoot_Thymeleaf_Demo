6c1f046d617049ae3b105102f6bab10b680b3202
DevanshShukla <devansh1352000@gmail.com>
2023-04-09 22:11:37 +0530

    add a public method sayHello() of type String

--- src/main/java/com/decoder135/springboot/thymeleafdemo/controller/DemoController.java
+++ src/main/java/com/decoder135/springboot/thymeleafdemo/controller/DemoController.java
@@ -1,7 +1,13 @@
 package com.decoder135.springboot.thymeleafdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 @Controller
 public class DemoController {
+
+    public String sayHello(Model theModel) {
+
+    }
+
 }
