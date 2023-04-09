44ca411dd7fbf8c12946adf435fbe763c06915c5
DevanshShukla <devansh1352000@gmail.com>
2023-04-09 22:12:03 +0530

    add the @GetMapping() annotation for the method sayHello()

--- src/main/java/com/decoder135/springboot/thymeleafdemo/controller/DemoController.java
+++ src/main/java/com/decoder135/springboot/thymeleafdemo/controller/DemoController.java
@@ -2,10 +2,12 @@ package com.decoder135.springboot.thymeleafdemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DemoController {
 
+    @GetMapping("/hello")
     public String sayHello(Model theModel) {
 
     }
